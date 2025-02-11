f82a5f72685de7cc142c165ec81d50bd68a74435
Ghitado <thiaguinho.020405@gmail.com>
2025-02-11 17:57:28 -0300

    feat: add product repository and entity configuration

---
+++ src/ControlaEstoque.API/Data/Repositories/Product/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ControlaEstoque.API.Data.Repositories.Product;
+
+public interface IProductRepository
+{
+    Task<Models.Product?> GetById(Guid id);
+    Task<IEnumerable<Models.Product>> GetAll();
+    Task Add(Models.Product product);
+    Task Update(Models.Product product);
+    Task Delete(Models.Product product);
+}
