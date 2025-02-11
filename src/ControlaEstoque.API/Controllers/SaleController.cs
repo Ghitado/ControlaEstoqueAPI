f336cc1ef0d5aace3e8e8f0fbf96d24eea543117
Ghitado <thiaguinho.020405@gmail.com>
2025-02-11 19:17:18 -0300

    feat: add sale controller

--- src/ControlaEstoque.API/Controllers/SaleController.cs
+++ src/ControlaEstoque.API/Controllers/SaleController.cs
@@ -1,8 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControlaEstoque.API.Models.DTOs;
+using ControlaEstoque.API.Services.Sale;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControlaEstoque.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class SaleController : ControllerBase
 {
+    private readonly ISaleService _service;
+
+    public SaleController(ISaleService service) => _service = service;
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById(
+        [FromRoute] Guid id,
+        CancellationToken cancellationToken)
+    {
+        var sale = await _service.GetById(id);
+        return sale != null ? Ok(sale) : NotFound();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAll()
+    {
+        var sales = await _service.GetAll();
+        return Ok(sales);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Add(
+        [FromBody] SaleInputDTO saleDto,
+        CancellationToken cancellationToken)
+    {
+        await _service.Add(saleDto);
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(
+        [FromRoute] Guid id,
+        [FromBody] SaleInputDTO saleDto,
+        CancellationToken cancellationToken)
+    {
+        await _service.Update(id, saleDto);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(
+        [FromRoute] Guid id,
+        CancellationToken cancellationToken)
+    {
+        await _service.Delete(id);
+        return NoContent();
+    }
 }
