b887d9c928788a22931d0d9d7dd672d54b77c792
Ghitado <thiaguinho.020405@gmail.com>
2025-02-11 18:05:10 -0300

    feat: add DbContext

---
+++ src/ControlaEstoque.API/Data/Context/ControlaEstoqueDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ControlaEstoque.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlaEstoque.API.Data.Context;
+
+public class ControlaEstoqueDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Sale> SaleItems { get; set; }
+    public DbSet<Sale> Sales { get; set; }
+
+    public ControlaEstoqueDbContext(DbContextOptions<ControlaEstoqueDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ControlaEstoqueDbContext).Assembly);
+    }
+}
